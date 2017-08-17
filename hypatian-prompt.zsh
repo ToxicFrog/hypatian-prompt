a6789c026fd7a3f8ba9ce97a8aa57b4e569933f1
Katherine Prevost <kat@hypatian.org>
2017-08-17 15:37:02 -0400

    Added copyright date and license file

--- hypatian-prompt.zsh
+++ hypatian-prompt.zsh
@@ -5,6 +5,8 @@
 
 # Requires zsh 5.3+
 
+# Copyright 2017 Katherine Anne Prevost
+#
 # This work is licensed under a Creative Commons Attribution 4.0
 # International License.
 # CC BY 4.0 (https://creativecommons.org/licenses/by/4.0/)
